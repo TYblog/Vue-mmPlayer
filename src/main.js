9f4876a8b5bae46822aad53855fd173bf64a78c0
TYblog <35173696+TYblog@users.noreply.github.com>
2023-06-17 10:19:27 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -39,19 +39,10 @@ router.beforeEach((to, from, next) => {
     next()
   } else {
     document.title =
-      (to.meta.title && `${to.meta.title} - mmPlayer在线音乐播放器`) || 'mmPlayer在线音乐播放器'
+      (to.meta.title && `${to.meta.title} - Player在线音乐播放器`) || 'Player在线音乐播放器'
     next()
   }
 })
-
-// 版权信息
-window.mmPlayer = window.mmplayer = `欢迎使用 mmPlayer!
-当前版本为：V${VERSION}
-作者：茂茂
-Github：https://github.com/maomao1996/Vue-mmPlayer
-歌曲来源于网易云音乐 (https://music.163.com)`
-// eslint-disable-next-line no-console
-console.info(`%c${window.mmplayer}`, `color:blue`)
 
 // eslint-disable-next-line no-new
 new Vue({
